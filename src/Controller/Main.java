cde563cc1fb32d454089f8b52c4cd9423db20a07
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-13 12:00:41 +0600

    initial commit

---
+++ src/Controller/Main.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Md. Sohan Millat Sakib
+ */
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/Root/Root.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
