b1d866d4f6fa59e2beacbfa3f940417405411b15
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-11-13 17:30:25 +0600

    Model added

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import Model.MYSQLDatabaseOp;
+import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,9 +15,25 @@ import javafx.stage.Stage;
 public class Main extends Application {
     
     @Override
+    
+    //database testing
     public void start(Stage primaryStage) throws Exception {
+        MYSQLDatabaseOp dbOp = new MYSQLDatabaseOp();
+        try {
+            
+            dbOp.handleQuery("vugijugi", "SELECT Roll,Name FROM students");
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        
         Parent root = FXMLLoader.load(getClass().getResource("/View/Auth/Base.fxml"));
         Scene scene = new Scene(root);
+        primaryStage.setMaxHeight(593);
+        primaryStage.setMaxWidth(990);
+        primaryStage.setMinWidth(930);
+        primaryStage.setMinHeight(490);
         primaryStage.setTitle("Hospital Management System");
         primaryStage.setScene(scene);
         primaryStage.show();
---
+++ src/Model/MYSQLDatabaseOp.java
@@ -0,0 +1,46 @@
+
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MYSQLDatabaseOp {
+    private final String URL;
+    private final String USERNAME;
+    private final String PASSWORD;
+    
+    
+    public MYSQLDatabaseOp() {
+        this.URL ="jdbc:mysql://localhost:3306";
+        this.USERNAME = "root";
+        this.PASSWORD = "";    
+    }
+    
+    
+    public MYSQLDatabaseOp(String URL, String USERNAME, String PASSWORD) {
+        this.URL = URL;
+        this.USERNAME = USERNAME;
+        this.PASSWORD = PASSWORD;
+    }
+    
+    public void handleQuery(String dbName, String sqlCommand) throws SQLException {
+        String fullURL = URL + "/" + dbName;
+        try (Connection connection = DriverManager.getConnection(fullURL, USERNAME, PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(sqlCommand)) {
+            if (sqlCommand.trim().toUpperCase().startsWith("SELECT")) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        String columnValue = resultSet.getString(1);
+                System.out.println("Column Value: " + columnValue);
+                    }
+                }
+            } else {
+                int affectedRows = statement.executeUpdate();
+                System.out.println("Affected rows: " + affectedRows);
+            }
+        }
+    }
+}
